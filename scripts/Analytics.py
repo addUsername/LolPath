606fb5773a519d0a095825bbad95e0e2ed082325
addUsername <60299373+addUsername@users.noreply.github.com>
2020-06-05 21:09:01 +0200

    Add files via upload

--- scripts/Analytics.py
+++ scripts/Analytics.py
@@ -7,7 +7,6 @@ class Analytics:
     champions = []
     champions_name = []
     champions_dict={}
-    frame_step=0
     
     scale_percent = 52 # percent of original size
     
@@ -45,13 +44,12 @@ class Analytics:
     def readVideo(self, threshold, second_inicial, frame_step, frame_stop):
         
         self.getChampions()
-        self.title+="threshold"+str(threshold)+"_noCNN"
-        self.frame_step=frame_step
-        print(self.frame_step)
+        self.title+="threshold"+str(threshold)+"_noCNN34"
         h_champ ,w_champ = self.champions[0].shape
         
         video_rgb = cv2.VideoCapture(self.video_path)
-        video_rgb.set(cv2.CAP_PROP_POS_FRAMES, (second_inicial*30)-1)
+        video_rgb.set(cv2.CAP_PROP_POS_FRAMES, int((second_inicial*30)))
+        #video_rgb.set(cv2.CAP_PROP_POS_FRAMES, 2000)
         
         
         _, frame = video_rgb.read()
@@ -68,85 +66,100 @@ class Analytics:
             
             if ret == True:
                 
-                if (fram_pos%frame_step==0 and fram_pos<frame_stop):
-                    print("loading frame..")
-                    #crop map area
-                    frame = frame[h1:h,w1:w]
-            
-                    #frame transformation
-                    frame = cv2.GaussianBlur(frame,(5,5),0)
-                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                    frame = cv2.Canny(frame,50,100,True)
-                    
-                    i=0
-                    for champ in (self.champions):
-                        #frame_compare = cv2.subtract(frame_canny,start)
-                        res = cv2.matchTemplate(frame,champ,cv2.TM_CCOEFF_NORMED)
+                if (fram_pos%frame_step==0):
+                    if (fram_pos<frame_stop):
+                        #crop map area
+                        frame = frame[h1:h,w1:w]
+                
+                        #frame transformation
+                        frame = cv2.GaussianBlur(frame,(5,5),0)
+                        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                        frame = cv2.Canny(frame,50,100,True)
                         
-                        _, max_val, _, max_loc = cv2.minMaxLoc(res)
-                        if(max_val > threshold):
-                            print("Yay")
-                            self.champions_dict[self.champions_name[i]].append([max_loc[0]+int(w_champ/2),max_loc[1]+int(h_champ/2)])
-                        else:
-                            self.champions_dict[self.champions_name[i]].append(None)
-                        i+=1
-                    i=0
-                elif (fram_pos>frame_stop):
-                    self.writeJSON()
-                    return 1                    
-                    
-                fram_pos+=1
+                        i=0
+                        for champ in (self.champions):
+                            #frame_compare = cv2.subtract(frame_canny,start)
+                            res = cv2.matchTemplate(frame,champ,cv2.TM_CCOEFF_NORMED)
+                            
+                            _, max_val, _, max_loc = cv2.minMaxLoc(res)
+                            if(max_val > threshold):
+                                self.champions_dict[self.champions_name[i]].append([max_loc[0]+int(w_champ/2),max_loc[1]+int(h_champ/2)])
+                            else:
+                                self.champions_dict[self.champions_name[i]].append(None)
+                            i+=1
+                        i=0
+                    elif (fram_pos>frame_stop):
+                        self.champions_dict["0frameStep"]=frame_step
+                        self.champions_dict["0seg,f_step,f_stop"]=[second_inicial, frame_step, frame_stop]
+                        print(self.champions_dict["0seg,f_step,f_stop"])
+                        self.writeJSON()
+                        return 1                    
+                        
+                    fram_pos+=1
             else:
                 break
         print("Lenght of first champion: "+str(len(self.champions_dict[self.champions_name[0]])))
-        print("Printing json: ..\\outputs\\"+self.title+".json")
+        print("Printing json: ..\\outputs\\"+self.title+"2.json")
+        self.champions_dict["0frameStep"]=frame_step
+        self.champions_dict["0seg,f_step,f_stop"]=[second_inicial, frame_step, frame_stop]
         self.writeJSON()
 
-    def writeVideo(self):
-        #falta hacer la lectura desde el json y no desde el atributo champions_path
-        video=[]
+    def writeVideo(self,jsonName):
+                        
+        self.title = jsonName
+        self.readJson()
+        self.champions_name = [name for name in self.champions_dict.keys() if "0" not in name]
+        print(self.champions_name)
         colors=[(255,0,0),(0,255,0),(0,0,255),(0,255,255),(255,255,0)] #red,green,blue,yellow
+        
+        fram_pos=0
+        ult_valid_position = [tuple((0,1)) for i in range (5)]
+        
+        
         video_rgb = cv2.VideoCapture(self.video_path)
-        
-        first = iter(self.champions_dict[self.champions_name[0]])
-        second = iter(self.champions_dict[self.champions_name[1]])
-        third = iter(self.champions_dict[self.champions_name[2]])
-        fourth = iter(self.champions_dict[self.champions_name[3]])
-        fifth = iter(self.champions_dict[self.champions_name[4]])
-        
-        iterators = [first,second,third,fourth,fifth]
-        
+        video_rgb.set(cv2.CAP_PROP_POS_FRAMES, int((self.champions_dict["0seg,f_step,f_stop"][0]*30)))
+        #video_rgb.set(cv2.CAP_PROP_POS_FRAMES, 2000)
         _, frame = video_rgb.read()
         
         h = frame.shape[0]
         w = frame.shape[1]
         h1 = int(h - h/4)
         w1 = int(w - w/7)
+        frame = frame[h1:h,w1:w]
+        print(frame.shape[1],frame.shape[0])
         
-        fram_pos=0
-        
+                
         print("escribiendo video")
+        print("Wirttin"+str(self.champions_dict[self.champions_name[3]][fram_pos]))
+        print((self.video_path[:-4]+self.title+'.avi'))
+        
+        out = cv2.VideoWriter(self.video_path[:-4]+self.title+'.avi',cv2.VideoWriter_fourcc(*'DIVX'), 30, (frame.shape[1],frame.shape[0]))
         while(video_rgb.isOpened()):
 
             ret, frame = video_rgb.read()
-            if ret == True:
-                if (fram_pos%self.frame_step==0):
-                    frame = frame[h1:h,w1:w]
+            if ((ret == True) and (fram_pos<self.champions_dict["0seg,f_step,f_stop"][2])):
+                
+                frame = frame[h1:h,w1:w]
+                if(fram_pos%self.champions_dict["0seg,f_step,f_stop"][1]==0):
                     i=0
                     for champ in (self.champions):
-                       position  = iterators[i].next()
-                       if(position!=None):
-                           cv2.circle(frame,(position[1],position[0]), 5, colors[i], -1)
+                       position  = self.champions_dict[self.champions_name[i]][fram_pos]
+                       if(position != None):
+                           ult_valid_position[i]=[position[0],position[1]]
+                           cv2.circle(frame,tuple((ult_valid_position[i])), 13, colors[i], 1)
+                       else:
+                           cv2.circle(frame,tuple((ult_valid_position[i])), 13, colors[i], 1)
                        i+=1
+                else:
                     i=0
-                    video.append(frame)
-                fram_pos=0
+                    for champ in (self.champions):
+                        cv2.circle(frame,tuple((ult_valid_position[i])), 13, colors[i], 1)
+                        i+=1
+                fram_pos+=1
+                out.write(frame)                    
             else:
                 break
-        
-        out = cv2.VideoWriter(os.getcwd()[:len(os.getcwd())-13]+"visualization\\outputs\\videos"+self.title+'.avi',cv2.VideoWriter_fourcc(*'DIVX'), 30, (video[0].shape[1],video[1].shape[0]))
-        for i in range(len(video)):
-            out.write(video[i])
+
         out.release()
         print("video escrito")
         
@@ -155,6 +168,12 @@ class Analytics:
         with open(os.getcwd()[:len(os.getcwd())-13]+"visualization\\outputs\\"+self.title+".json","w") as file: 
             json.dump(self.champions_dict, file)
             
+    def readJson(self):
+        #Asi nos saltamos tener q analizar el video, lo pillamos de aqui e ya.
+        with open(os.getcwd()[:len(os.getcwd())-13]+"visualization\\outputs\\"+self.title+".json","r") as file:
+            self.champions_dict = json.load(file)
+            #print(self.champions_dict)
+
     
     
    
--- scripts/Plotting.py
+++ scripts/Plotting.py
@@ -10,42 +10,51 @@ from matplotlib import cbook
 import math
 import json
 
-def __init__(self,title,champions):
-    
-    self.title = title
-    self.champions = champions
-    self.data={}
-    
-    
-def readJson(self):
-
-    with open(self.title+".json") as json_file:
-        self.data = json.load(json_file)
+class Plotting:
         
-def dist(a,b):
-    return math.sqrt(math.pow(a[0]-b[0],2)+math.pow(a[1]-b[1],2))
+    
+    def __init__(self,title,champions):
         
-def generatePlot(self, dist, num_points):
+        self.title = title
+        self.champions = champions
+        self.champions_dict={}
+        
+        
+    def writeJSON(self):
+        
+        with open(os.getcwd()[:len(os.getcwd())-13]+"visualization\\outputs\\"+self.title+".json","w") as file: 
+            json.dump(self.champions_dict, file)
+            
+    def readJson(self):
+        #Asi nos saltamos tener q analizar el video, lo pillamos de aqui e ya.
+        with open(os.getcwd()[:len(os.getcwd())-13]+"visualization\\outputs\\"+self.title+".json","r") as file:
+            self.champions_dict = json.load(file)
+            #print(self.champions_dict)
     
-    imageFile = cbook.get_sample_data("..\\resources\\inicial.png")
-    image = plt.imread(imageFile)
-    plt.imshow(image)
-    plt.suptitle(self.title, fontsize=14, fontweight='bold')
-
+    def dist(a,b):
+        return math.sqrt(math.pow(a[0]-b[0],2)+math.pow(a[1]-b[1],2))
+            
+    def generatePlot(self, dist, num_points):
+        
+        imageFile = cbook.get_sample_data("..\\resources\\inicial.png")
+        image = plt.imread(imageFile)
+        plt.imshow(image)
+        plt.suptitle(self.title, fontsize=14, fontweight='bold')
     
-    x=[]
-    y=[]
-    num=0
-    pos_pre=self.paths[self.champions[0]][0]    
-    for i in range(1,len(self.paths[self.champions[0]])-1):
-        if((dist(pos_pre, self.paths[self.champions[0][i]])) < dist):
-            pos_pre=self.paths[self.champions[0][i]]
-                               
-            x.append(pos_pre[0])
-            y.append(pos_pre[1])
-        elif (num==num_points):
-            pos_pre=self.paths[self.champions[0][i]]
-            num=0 #???
-        num+=1
-    plt.plot(x,y,'b-',label="amumu")
-
+        
+        x=[]
+        y=[]
+        num=0
+        pos_pre=self.paths[self.champions[0]][0]    
+        for i in range(1,len(self.paths[self.champions[0]])-1):
+            if((dist(pos_pre, self.paths[self.champions[0][i]])) < dist):
+                pos_pre=self.paths[self.champions[0][i]]
+                                   
+                x.append(pos_pre[0])
+                y.append(pos_pre[1])
+            elif (num==num_points):
+                pos_pre=self.paths[self.champions[0][i]]
+                num=0 #???
+            num+=1
+        plt.plot(x,y,'b-',label="amumu")
+    
